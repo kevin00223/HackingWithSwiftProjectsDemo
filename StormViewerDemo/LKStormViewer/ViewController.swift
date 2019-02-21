ff7e1d1ee3b06a8950b2ac41de1a3ba9fb277f72
李凯 <likai.new@gmail.com>
2019-02-21 11:46:13 +0800

    提交图片资源 获取图片资源 并全局保存

--- StormViewerDemo/LKStormViewer/ViewController.swift
+++ StormViewerDemo/LKStormViewer/ViewController.swift
@@ -6,15 +6,31 @@
 //  Copyright © 2019年 LK. All rights reserved.
 //
 
+/*
+ 1. 拿到图片资源 并全局保存
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+        print("arr's count is \(pictures.count)")
     }
-
-
 }
 
