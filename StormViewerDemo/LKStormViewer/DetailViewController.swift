3d6c0a4aa2c148374d086017b016e9a5e33899d3
李凯 <likai.new@gmail.com>
2019-02-21 18:06:17 +0800

    完成点击cell跳转图片浏览控制器展示图片

---
+++ StormViewerDemo/LKStormViewer/DetailViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DetailViewController.swift
+//  LKStormViewer
+//
+//  Created by 李凯 on 2019/2/21.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        title = selectedImage
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        // selectedImage是可选型, 因此需要if let解包
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- StormViewerDemo/LKStormViewer/ViewController.swift
+++ StormViewerDemo/LKStormViewer/ViewController.swift
@@ -12,7 +12,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var pictures = [String]()
 
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        title = "Storm Viewer"
+        
+        //1.获取图片资源 并全局保存
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -29,8 +34,29 @@ class ViewController: UIViewController {
                 pictures.append(item)
             }
         }
-        print(pictures)
-        print("arr's count is \(pictures.count)")
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row] //textLabel? : optional chaining
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //1. 每个控制器都有一个storyboard属性, 但swift不确定知道该storyboard是否有值 因此需要optional chaining (try doing this, but do nothing if there is a problem)
+        //2. 通过instantiateViewController()获取到对应的控制器
+        //3. 由于instantiateViewController()方法返回的结果类型为UIViewController, 因此需要通过typeCasting(以optionalDowncast的形式)将返回值类型转化成DetailViewController类 并通过safely unwrapping optionals(if let)解包
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
 }
 
