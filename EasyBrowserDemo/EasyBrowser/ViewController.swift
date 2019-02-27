33d73350284bde11cf69b292517d858e35f136c0
李凯 <likai.new@gmail.com>
2019-02-27 11:53:00 +0800

    1. Webview load url request 2. Add an alertController

---
+++ EasyBrowserDemo/EasyBrowser/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by 李凯 on 2019/2/27.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    var webView: WKWebView! //implicitly unwrapping optional
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.backgroundColor = .blue
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        initNavigationBar()
+        
+        let url = URL(string: "https://www.baidu.com/")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func initNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    }
+    
+    @objc func openTapped() {
+        let alert = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "pan.baidu.com", style: .default, handler: { (alertAction) in
+            self.openPage(alertAction)
+        }))
+        alert.addAction(UIAlertAction(title: "jianshu.com", style: .default, handler: { (alertAction) in
+            self.openPage(alertAction)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func openPage(_ alertAction: UIAlertAction) {
+        guard let urlString = alertAction.title else { return }
+        guard let url = URL(string: "https://\(urlString)") else { return }
+        webView.load(URLRequest(url: url))
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+}
+
