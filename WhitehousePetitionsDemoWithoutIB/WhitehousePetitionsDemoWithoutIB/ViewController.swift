a2689202543f5c675ebd72384e1b611aa1148da1
李凯 <likai.new@gmail.com>
2019-03-08 17:02:10 +0800

    Challenge done

--- WhitehousePetitionsDemoWithoutIB/WhitehousePetitionsDemoWithoutIB/ViewController.swift
+++ WhitehousePetitionsDemoWithoutIB/WhitehousePetitionsDemoWithoutIB/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    
+    /// 最终要展示的数据源
+    var dataSourcePetitions = [Petition]()
+    
+    /// 全部数据源
     var petitions = [Petition]()
+    
+    /// 与用户输入相匹配的数据源
+    var matchedPetitions = [Petition]()
+    
     var urlString = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         initValue()
+        initNavigationBar()
         requestData()
     }
     
@@ -28,6 +38,10 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cellID")
+    }
+    
+    func initNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(rightBarButtonItemOnTap))
     }
     
     func requestData() {
@@ -42,7 +56,8 @@ class ViewController: UITableViewController {
     func parse(json: Data) {
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-           petitions = jsonPetitions.results
+            petitions = jsonPetitions.results
+            dataSourcePetitions = petitions
         }
     }
     
@@ -52,20 +67,51 @@ class ViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    @objc func rightBarButtonItemOnTap() {
+//        let alertController = UIAlertController(title: "Note", message: "Data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+//        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//        present(alertController, animated: true, completion: nil)
+        
+        dataSourcePetitions = petitions
+        tableView.reloadData()
+        matchedPetitions.removeAll()
+        
+        let alertController = UIAlertController(title: "Filter", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        alertController.addAction(UIAlertAction(title: "Submit", style: .default, handler: { (alertAction) in
+            if let submitString = alertController.textFields?[0].text {
+                for item in self.petitions {
+                    if item.title.contains(submitString) {
+                        self.matchedPetitions.append(item)
+                    }
+                }
+                if !self.matchedPetitions.isEmpty {
+                    self.dataSourcePetitions = self.matchedPetitions
+                    self.tableView.reloadData()
+                }else{
+                    let alertController = UIAlertController(title: "No petition matched, please try again", message: nil, preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return dataSourcePetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
-        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.textLabel?.text = dataSourcePetitions[indexPath.row].title
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = dataSourcePetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
