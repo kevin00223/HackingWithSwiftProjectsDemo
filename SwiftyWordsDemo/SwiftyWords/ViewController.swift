625098f24f402c8fb8773890094a785a71835e8d
李凯 <likai.new@gmail.com>
2019-03-13 10:33:23 +0800

    Init project

---
+++ SwiftyWordsDemo/SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by 李凯 on 2019/3/13.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
