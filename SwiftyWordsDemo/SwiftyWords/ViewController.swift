7c902554d9bd4607fabb66d9a1ec4101c27cf1a6
李凯 <likai.new@gmail.com>
2019-03-14 17:23:35 +0800

    Add code to async long-running operation on background thread and get back to foreground to update user interface

--- SwiftyWordsDemo/SwiftyWords/ViewController.swift
+++ SwiftyWordsDemo/SwiftyWords/ViewController.swift
@@ -44,24 +44,28 @@ class ViewController: UIViewController {
         var solutionString = ""
         var letterBits = [String]()
         
-        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
-            if let levelContents = try? String(contentsOf: levelFileURL) {
-                var lines = levelContents.components(separatedBy: "\n")
-                lines.shuffle()
-                
-                for(index, line) in lines.enumerated() {
-                    let parts = line.components(separatedBy: ":")
-                    let answer = parts[0]
-                    let clue = parts[1]
+        DispatchQueue.global().async { [weak self] in
+            if let levelFileURL = Bundle.main.url(forResource: "level\(String(describing: self?.level))", withExtension: "txt") {
+                if let levelContents = try? String(contentsOf: levelFileURL) {
+                    var lines = levelContents.components(separatedBy: "\n")
+                    lines.shuffle()
                     
-                    clueString += "\(index + 1).\(clue)\n"
-                    
-                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
-                    solutionString += "\(solutionWord.count) letters\n"
-                    solutions.append(solutionWord)
-                    
-                    let bits = answer.components(separatedBy: "|")
-                    letterBits += bits
+                    DispatchQueue.main.async { [weak self] in
+                        for(index, line) in lines.enumerated() {
+                            let parts = line.components(separatedBy: ":")
+                            let answer = parts[0]
+                            let clue = parts[1]
+                            
+                            clueString += "\(index + 1).\(clue)\n"
+                            
+                            let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                            solutionString += "\(solutionWord.count) letters\n"
+                            self?.solutions.append(solutionWord)
+                            
+                            let bits = answer.components(separatedBy: "|")
+                            letterBits += bits
+                        }
+                    }
                 }
             }
         }
