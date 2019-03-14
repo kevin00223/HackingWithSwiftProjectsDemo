afcec356d075a17dbb91db110f465b2b7a414023
李凯 <likai.new@gmail.com>
2019-03-14 11:36:03 +0800

    copy from Project7 WhitehousePetitions

---
+++ GrandCentralDispatchDemo/WhitehousePetitionsDemo/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  WhitehousePetitionsDemo
+//
+//  Created by 李凯 on 2019/3/7.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
