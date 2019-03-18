dbd01143725f1e827d0676990fa301d4b8869b07
李凯 <likai.new@gmail.com>
2019-03-18 11:48:49 +0800

    Custom collectionViewCell

---
+++ NamesToFacesDemo/NamesToFacesDemo/PersonCell.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCell.swift
+//  NamesToFacesDemo
+//
+//  Created by 李凯 on 2019/3/18.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
