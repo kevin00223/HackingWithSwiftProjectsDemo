c59b1dfc052cb016357ff3e23557d5697d49e991
李凯 <likai.new@gmail.com>
2019-04-09 10:31:20 +0800

    Set up interface

---
+++ InstafilterDemo/InstafilterDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  InstafilterDemo
+//
+//  Created by 李凯 on 2019/4/9.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var intensity: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
+    
+}
+
