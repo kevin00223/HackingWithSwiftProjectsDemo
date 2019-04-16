d288829e045a5d8f9f3c71ff835304a151097179
李凯 <likai.new@gmail.com>
2019-04-16 11:03:57 +0800

    Init project

---
+++ WhackAPenguinDemo/Whack-A-Penguin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whack-A-Penguin
+//
+//  Created by 李凯 on 2019/4/16.
+//  Copyright © 2019年 LK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
